c42befa5c6027dd1f274e29739174057234c80cf
Gavin Wilson <gmanwilson@icloud.com>
2025-04-18 13:23:29 -0400

    added database schemas and User database logic

--- app.py
+++ app.py
@@ -11,5 +11,48 @@ jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 CORS(app)
 
+# Table to store User data
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    _password_hashed = db.Column(db.String(60))
+
+    @property
+    def password(self):
+        raise AttributeError('Password field is write-only')
+
+    @property.setter
+    def password(self, password):
+        self._password_hashed = bcrypt.generate_password_hash(
+            password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self._password_hashed, password)
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+    # Table to store list of exercises
+
+
+class Exercises(db.Model):
+    pass
+
+# Table to store User's workouts
+
+
+class Workouts(db.Model):
+    pass
+
+# Table to store details about workouts
+
+
+class Workout_Exercises(db.Model):
+    pass
+
+
 if __name__ == '__main__':
     app.run()
